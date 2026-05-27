755800f2a23940112c9727c57c22010018348cbe
pikitosdoritos <fastovetsn@gmail.com>
2026-05-27 18:51:53 +0300

    Implement user login functionality with error handling and add password hashing utilities

--- backend/endpoints/login.py
+++ backend/endpoints/login.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI, Depends, APIRouter
+from fastapi import FastAPI, Depends, APIRouter, HTTPException
 from .. import models, schemas
+from sqlalchemy.orm import Session
+from backend.database import get_db
 
-router = APIRouter(prefix="login", tags=["login"])
+router = APIRouter(prefix="/login", tags=["login"])
 
-@router.post("/user/login", response_model=)
+@router.post("/user/login", response_model=schemas.LogIn)
+def login_user(payload: schemas.LogIn, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(
+        models.User.email == payload.email, 
+        models.User.password == payload.password,
+    ).first()
+
+    if user:
+        return {"LogIn": "Succesful"}
+    else:
+        raise HTTPException(status_code=400, detail="Invalid email or password")
--- backend/endpoints/register.py
+++ backend/endpoints/register.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from backend.database import get_db
 from .. import schemas, models
+from hashlib import hash_password
 
 router = APIRouter(prefix="/register", tags=["register"])
 
 @router.post("/user/create", response_model=schemas.CreateUser)
 def create_user(payload: schemas.CreateUser, db: Session = Depends(get_db)):
+
     user = models.User(
         name = payload.name,
         email = payload.email,
