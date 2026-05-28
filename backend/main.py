201a555498ab047eafa59855c2938c9296726dc9
pikitosdoritos <fastovetsn@gmail.com>
2026-05-28 17:20:21 +0300

    Enhance backend and frontend integration by adding static file serving and updating form handling for sign-in and sign-up

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,27 @@ from fastapi import FastAPI
 from .endpoints import register, login
 from backend.database import engine
 from backend.models import Base
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+BASE_DIR = Path(__file__).resolve().parent
+
+FRONTEND_DIR = BASE_DIR.parent / "frontend"
+
 app.include_router(register.router)
 app.include_router(login.router)
+
+app.mount("/frontend", StaticFiles(directory=FRONTEND_DIR), name="frontend")
+
+@app.get("/")
+def read_index():
+    return FileResponse(FRONTEND_DIR / "index.html")
 
 @app.get("/")
 def hello_world():
--- frontend/script.js
+++ frontend/script.js
@@ -6,3 +6,19 @@ function switchForm(e) {
     signinForm.hidden =  !signinForm.hidden
     signupForm.hidden = !signupForm.hidden
 }
+
+function handleSignIn(e) {
+    e.preventDefault()
+    const data = Object.fromEntries(new FormData(signinForm))
+    const init = {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(data)}
+
+    fetch("/login/user", init)
+}
+
+function handleSignUp(e) {
+    e.preventDefault()
+    const data = Object.fromEntries(new FormData(signupForm))
+    const init = {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(data)}
+
+    fetch("/register/user", init)
+}
